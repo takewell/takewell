78d8338b337b68b4ee0de0502df7a47239475cdb
yuya takei <takewell.dev@gmail.com>
2020-12-14 01:44:40 +0900

    Add description

--- website/data/site.js
+++ website/data/site.js
@@ -1,7 +1,7 @@
 export const SITE = {
   title: "@takewell - yuya.takei",
   description:
-    "",
+    "Software engineer,愛媛県松山市育ち、工学部を卒業後 千葉のファッションテック企業勤めZ ガンダムが好き。熱意と愛情と野心に満ちた成熟した人間を目指しています。",
   twitter: "_takewell",
   domain: "takewell.dev",
   email: `mailto:takewell.dev@gmail.com`,
